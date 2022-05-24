60665d83cfe956b5f6118781456776d38cb56e7c
j4k3ng <41842937+j4k3ng@users.noreply.github.com>
2022-05-24 12:47:58 +0100

    getEthereum

--- client/src/getEthereum.js
+++ client/src/getEthereum.js
@@ -1,10 +1,10 @@
-// export const getEthereum = async () => {
+export const getEthereum = async () => {
 
-//     // event listener is not reliable
-//     while (document.readyState !== "complete") {
-//         await new Promise(resolve => setTimeout(resolve, 100))
-//     }
-//     console.log(window.ethereum)
-//     return window.ethereum
+    // event listener is not reliable
+    while (document.readyState !== "complete") {
+        await new Promise(resolve => setTimeout(resolve, 100))
+    }
+    console.log(window.ethereum)
+    return window.ethereum
 
-// }
+}
